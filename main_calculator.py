ec7c609a3582c6b80742983525855b98f58f43be
Dale Menciro <dale_menciro@protonmail.com>
2023-05-22 00:13:54 +0800

    calling the calculatorapp and run it

--- main_calculator.py
+++ main_calculator.py
@@ -2,3 +2,7 @@
 This file is for calling the GUI and running the main program
 '''
 from ui_calculator import CalculatorApp
+
+if __name__ == "__main__":
+    app = CalculatorApp()
+    app.mainloop
