486f451a853e9de04b436e2af0c8fc0946a56c8e
Dale Menciro <dale_menciro@protonmail.com>
2023-05-22 00:34:24 +0800

    adding ()

--- main_calculator.py
+++ main_calculator.py
@@ -2,7 +2,6 @@
 This file is for calling the GUI and running the main program
 '''
 from ui_calculator import CalculatorApp
-
 if __name__ == "__main__":
-    app = CalculatorApp()
+    app = CalculatorApp ()
     app.mainloop
