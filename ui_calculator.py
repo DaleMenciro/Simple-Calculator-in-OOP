95e42e9a820902a209a890e83cda6e83e8a37388
Dale Menciro <dale_menciro@protonmail.com>
2023-05-21 23:29:04 +0800

    setting up the GUI

--- ui_calculator.py
+++ ui_calculator.py
@@ -10,4 +10,14 @@ from operations_calculator import Operation
 class CalculatorApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        
+        self.title("Simple App Calculator")
+        self.geometry("350x350")
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+        self.rowconfigure(3, weight=1)
+        self.rowconfigure(4, weight=1)
+        self.rowconfigure(5, weight=1)
+        self.rowconfigure(6, weight=1)
