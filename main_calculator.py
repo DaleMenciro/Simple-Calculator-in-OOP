9ae706b2e13f12c419b3de77e1cef02e7a861e54
Dale Menciro <dale_menciro@protonmail.com>
2023-05-22 00:34:50 +0800

    fixed error

--- main_calculator.py
+++ main_calculator.py
@@ -3,5 +3,5 @@ This file is for calling the GUI and running the main program
 '''
 from ui_calculator import CalculatorApp
 if __name__ == "__main__":
-    app = CalculatorApp ()
-    app.mainloop
+    app = CalculatorApp()
+    app.mainloop()
