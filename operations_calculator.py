024df48a57a5c8ff8370e73095ea1c7a05cac2e7
Dale Menciro <dale_menciro@protonmail.com>
2023-05-21 12:15:34 +0800

    creating operation methods

--- operations_calculator.py
+++ operations_calculator.py
@@ -6,3 +6,10 @@ This file is for the operation methods
 #create class
 class Operation:
 #create methods for operations
+    def add(self):
+    
+    def subtract(self):
+
+    def multiply(self):
+
+    def divide(self):
