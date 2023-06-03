51cd4cc24cd6f920d68390f3ea1d04066f6811fb
Dale Menciro <dale_menciro@protonmail.com>
2023-06-03 21:15:11 +0800

    testing

--- additional_operations_calculator.py
+++ additional_operations_calculator.py
@@ -32,4 +32,15 @@ class AdditionalOperation:
     def sin(self, angle):
         return math.sin(math.radians(angle))
         
-    
+if __name__ == "__main__":
+    math_operation = AdditionalOperation()
+
+    print(math_operation.square(5))  # Output: 25
+
+    print(math_operation.cube(3))  # Output: 27
+
+    print(math_operation.square_root(16))  # Output: 4.0
+
+    print(math_operation.power(2, 4))  # Output: 16.0
+
+    print(math_operation.sin(30))  # Output: 0.49999999999999994
