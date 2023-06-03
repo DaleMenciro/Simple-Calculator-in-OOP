c2c804f281673819e15c52388a3d5fe3c8b27dc1
Dale Menciro <dale_menciro@protonmail.com>
2023-06-03 21:01:28 +0800

    Adding comments for guidance

---
+++ additional_operations_calculator.py
@@ -0,0 +1,10 @@
+'''
+This file inherits from the Operation class 
+from operations_calculator.py file
+
+This file adds additional operation methods
+such as: square, cube, square root, cube root,
+power, and sin.
+
+'''
+
